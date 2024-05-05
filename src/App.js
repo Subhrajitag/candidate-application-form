1519cb0f610f6af1572d24f999ce45c137d18238
Subhrajita Garnayak (NTE-592) <subhrajita.garnayak@nettantra.net>
2024-05-05 11:58:42 +0530

    job cards UI updated

--- src/App.js
+++ src/App.js
@@ -1,51 +1,68 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import {
+  Grid,
+  makeStyles,
+  Container,
+} from "@material-ui/core";
 import "./App.css";
 import Filter from "./components/Filter";
 import JobCard from "./components/JobCard";
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  progress: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: theme.spacing(10),
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   const [jobs, setJobs] = useState([]);
   const [offset, setOffset] = useState(0);
-  const myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-
-  const raw = JSON.stringify({
-    limit: 10,
-    offset: offset,
-  });
-
-  const requestOptions = {
-    method: "POST",
-    headers: myHeaders,
-    body: raw,
-    redirect: "follow",
-  };
-
-  const fetchJobs = async () => {
-    try {
-      const response = await fetch(
-        "https://api.weekday.technology/adhoc/getSampleJdJSON",
-        requestOptions
-      )
-        .then((response) => response.json())
-        .then((result) => {
-          setJobs((prevJobs) => [...prevJobs, ...result.jdList]);
-        })
-        .catch((error) => console.log(error));
-    } catch (error) {
-      console.error("Error fetching job listings:", error);
-    }
-  };
 
   useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const response = await fetch(
+          "https://api.weekday.technology/adhoc/getSampleJdJSON",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              limit: 10,
+              offset: offset,
+            }),
+          }
+        );
+        const data = await response.json();
+        setJobs((prevJobs) => [...prevJobs, ...data.jdList]);
+      } catch (error) {
+        console.error("Error fetching job listings:", error);
+      }
+    };
+
     fetchJobs();
   }, [offset]);
 
   return (
     <div className="App">
       {/* <Filter/> */}
-      {console.log(jobs)}
-      {/* {jobs && jobs?.map((job, index) => <JobCard job={job} key={index} />)} */}
+      <Container maxWidth="lg" className={classes.container}>
+        <Grid container spacing={3}>
+          {jobs.map((job, index) => (
+            <Grid item xs={12} sm={6} md={4} key={index}>
+              <JobCard job={job} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </div>
   );
 }
--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,9 +1,106 @@
-import React from 'react'
+import React from "react";
+import {
+  Card,
+  CardContent,
+  Typography,
+  makeStyles,
+  CardActions,
+  Button,
+  Avatar,
+  Link,
+} from "@material-ui/core";
 
-const JobCard = () => {
+const JobCard = ({ job }) => {
+  const {
+    companyName,
+    jobRole,
+    location,
+    minJdSalary,
+    maxJdSalary,
+    salaryCurrencyCode,
+    jobDetailsFromCompany,
+    logoUrl,
+    jdLink,
+    minExp,
+    maxExp,
+  } = job;
+
+  const classes = useStyles();
+
   return (
-    <div>JobCard</div>
-  )
-}
+    <Card className={classes.card}>
+      <div className={classes.header}>
+        <Avatar alt={companyName} src={logoUrl} className={classes.avatar} />
+        <div className={classes.titleContainer}>
+          <Typography variant="h6" component="h2" className={classes.title}>
+            {jobRole}
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            color="textSecondary"
+            className={classes.subtitle}
+          >
+            {companyName} - {location}
+          </Typography>
+        </div>
+      </div>
+      <CardContent>
+        <Typography variant="body1" className={classes.salary}>
+        Estimated Salary : {minJdSalary? `${minJdSalary} - ` : ""} {maxJdSalary} {salaryCurrencyCode}
+        </Typography>
+        <Typography variant="body2" paragraph>
+          <strong>Experience Required:</strong> {minExp} - {maxExp} years
+        </Typography>
+        <Typography variant="body2" paragraph>
+          {jobDetailsFromCompany}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          size="small"
+          color="primary"
+          component={Link}
+          href={jdLink}
+          target="_blank"
+        >
+          Apply Now
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
 
-export default JobCard
+const useStyles = makeStyles((theme) => ({
+  card: {
+    marginBottom: theme.spacing(3),
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+    transition: "0.3s",
+    borderRadius: "8px",
+    overflow: "hidden",
+  },
+  header: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(2),
+  },
+  avatar: {
+    marginRight: theme.spacing(2),
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+  titleContainer: {
+    flexGrow: 1,
+  },
+  title: {
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontStyle: "italic",
+  },
+  salary: {
+    color: "#3f51b5",
+    marginBottom: theme.spacing(1),
+  },
+}));
+
+export default JobCard;
